11345104896d9adeded67befc44ed56f71680219
WASHIUL ISLAM <rashob@TUFxRASHOB>
2022-09-30 14:12:55 +0530

    EEEE

---
+++ for.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main () {
+
+            int num;
+
+                printf("Enter a Number");
+                    scanf("%d",&num);
+
+                        for(int i;i<num; i++)
+                        printf("%d\n",i);
+
+                        return 0;
+
+}
